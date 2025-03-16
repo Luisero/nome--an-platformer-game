383cca997f33e9ce05920db76638c4913aabdd65
Luis Henrique <luismoura2005@gmail.com>
2025-03-16 20:41:04 -0300

    FIX: move spritesheet to game folder

--- Assets/Sprites/Player-Sheet_without Dash Effekt.tsx
+++ Assets/Sprites/Player-Sheet_without Dash Effekt.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Player-Sheet_without Dash Effekt" tilewidth="64" tileheight="16" tilecount="34" columns="34">
- <image source="../../../../../Downloads/Player-Sheet_without Dash Effekt.png" width="2176" height="16"/>
+ <image source="Player-Sheet_without Dash Effekt.png" width="2176" height="16"/>
 </tileset>
